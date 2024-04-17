5d572195b3ce003002339d5045ac328062221443
BalaM14 <balamurugan141196@gmail.com>
2024-04-18 00:12:03 +0530

    Logger & Exception

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
-
+from income.logger import logging
+from income.exception import IncomeException
+import sys
 app=Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/",methods=['GET','POST'])
+def index():
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        income = IncomeException(e,sys)
+        logging.info(income.error_message)
+        logging.info("Testing the logging module")
+
     return "Hey Bala"
 
 if __name__=="__main__":
